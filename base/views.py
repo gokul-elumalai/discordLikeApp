e3281ca39e28c171ef2084f5ace6d1a79b85201a
gokul-elumalai <src.mv1f3@aleeas.com>
2024-09-22 12:21:09 +0530

    Add Base view mehtods and routes

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def room(request):
+    return HttpResponse("Room")
