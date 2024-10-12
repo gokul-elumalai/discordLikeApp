3d57bd8a2d6ea08b4efcadca1404682feb5c6724
gokul-elumalai <src.mv1f3@aleeas.com>
2024-10-12 20:36:12 +0530

    Add mobile responsiveness

--- base/urls.py
+++ base/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     path('delete-message/<str:pk>', views.delete_message, name='delete-message'),
 
     path('update-user/', views.update_user, name='update-user'),
+
+    path('topics/', views.topics_page, name='topics'),
+    path('activity/', views.activity_page, name='activity'),
 ]
